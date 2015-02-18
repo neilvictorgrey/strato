d467eee7f9fde778a9e5931afaaebef39b2f7d88
Neil Grey <neilg@image-engine.com>
2015-02-18 14:50:28 -0800

    Update __init__.py
    Removing invalid variable assignment on pass-through

--- __init__.py
+++ __init__.py
@@ -3,4 +3,4 @@ from core.Database import Types as datatypes
 from core.Entity.EntityBase import EntityBase
 
 def getConnection(dbType, location, port=None, dbName=None, username=None, password=None, echo=False):
-    return Connection(dbType, location, port=None, dbName=None, username=None, password=None, echo=False)
+    return Connection(dbType, location, port, dbName, username, password, echo)
